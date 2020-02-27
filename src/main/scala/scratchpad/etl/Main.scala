e5dbda81fac0270bd911a453428ba8a436817843
Peter Nham <peter.nham@rea-group.com>
2020-02-27 16:31:53 +1100

    Clean up stubs

--- src/main/scala/scratchpad/etl/Main.scala
+++ src/main/scala/scratchpad/etl/Main.scala
@@ -1,10 +1,8 @@
 package scratchpad.etl
 
-import scala.io.StdIn._
-
 object Main extends App {
 
- private def extract(): List[String] = ???
- private def transform(fruit: String): Int = ???
- private def
+  private def extract(): List[String] = ???
+  private def transform(value: String): Int = ???
+  private def load(value: Int): Unit = ???
 }
