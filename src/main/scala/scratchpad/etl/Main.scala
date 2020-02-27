efa8a31db8616d95b6889583023ace01aec2caed
Peter Nham <peter.nham@rea-group.com>
2020-02-27 16:30:26 +1100

    Thinking about an etl exercise

---
+++ src/main/scala/scratchpad/etl/Main.scala
@@ -0,0 +1,10 @@
+package scratchpad.etl
+
+import scala.io.StdIn._
+
+object Main extends App {
+
+ private def extract(): List[String] = ???
+ private def transform(fruit: String): Int = ???
+ private def
+}
