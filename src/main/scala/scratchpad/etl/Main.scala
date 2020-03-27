437cb6e5d320be89dfb2fd1cccdb2c1679487ba2
Peter Nham <peter.nham@rea-group.com>
2020-03-27 15:23:11 +1100

    Clean up notes

--- src/main/scala/scratchpad/etl/Main.scala
+++
@@ -1,8 +0,0 @@
-package scratchpad.etl
-
-object Main extends App {
-
-  private def extract(): List[String] = ???
-  private def transform(value: String): Int = ???
-  private def load(value: Int): Unit = ???
-}
