37e3cda011847081db6b13594ee18b9609784b61
Peter Nham <peter.nham@rea-group.com>
2020-03-27 15:23:25 +1100

    Add breakdown for fs2 tracker

--- src/main/scala/scratchpad/stream/Fs2TrackerInSteps.scala
+++ src/main/scala/scratchpad/stream/Fs2TrackerInSteps.scala
@@ -1,7 +1,9 @@
 package scratchpad.stream
 
+import cats.effect.concurrent.Ref
 import cats.effect.{Concurrent, Timer}
 import fs2.{Pipe, Stream}
+import cats.implicits._
 
 import scala.concurrent.duration.FiniteDuration
 
@@ -15,11 +17,22 @@ object Fs2TrackerInSteps {
   ): Pipe[F, A, A] = (in: Stream[F, A]) => {
 
     //Create a stream from a Ref[F, Int]
+    Stream.eval(Ref[F].of(0))
 
     //Logging stream
+    Stream.eval(Ref[F].of(0)).flatMap { countRef =>
+      Stream.repeatEval(countRef.get.flatMap(log)).metered(freq)
+    }
 
     //Update the count
+    Stream.eval(Ref[F].of(0)).flatMap { countRef =>
+      val stream1: Stream[F, A] = in.chunks
+        .evalMap(chunk => countRef.update(_ + chunk.size).map(_ => chunk))
+        .flatMap(Stream.chunk)
 
-    ???
+      val loggingStream: Stream[F, Unit] = Stream.repeatEval(countRef.get.flatMap(log)).metered(freq)
+
+      stream1.concurrently(loggingStream)
+    }
   }
 }
